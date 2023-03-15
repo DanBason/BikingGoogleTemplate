0fadac0d1d5ad59ba1ec7b13b37a79156f05d674
Daniel Bason <dbason@gmail.com>
2023-03-15 12:16:44 -0700

    dog

--- app/routes/default.py
+++ app/routes/default.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template
 
 # This is for rendering the home page
+
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
@@ -9,3 +10,7 @@ def aboutus():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/profilemy')
+def profile():
+    return render_template('profilemy.html')
