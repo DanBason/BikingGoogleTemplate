5e11655e0636559b36281bda2743638182eeb5f9
Daniel Bason <dbason@gmail.com>
2023-03-20 21:04:55 -0700

    buncha stuff

--- app/routes/default.py
+++ app/routes/default.py
@@ -8,6 +8,7 @@ from flask_login import (
     logout_user,
 )
 from oauthlib.oauth2 import WebApplicationClient
+import sqlite3
 
 
 # This is for rendering the home page
@@ -47,5 +48,38 @@ def message():
 def collegelist():
     return render_template('collegelist.html')
 
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    name = request.form['name']
+    email = request.form['email']
+    bio = request.form['bio']
+    
+    # Handle profile picture upload
+    if 'profile_picture' in request.files:
+        profile_picture = request.files['profile_picture'].read()
+    else:
+        profile_picture = None
+    
+    # Insert user data into the database
+    conn = sqlite3.connect('users.db')
+    cur = conn.cursor()
+    cur.execute('INSERT INTO users (name, email, bio, profile_picture) VALUES (?, ?, ?, ?)', 
+                (name, email, bio, profile_picture))
+    conn.commit()
+    conn.close()
+    
+    # Redirect to the user's profile page
+    return redirect(url_for('profile', user_id=cur.lastrowid))
+
+
+@app.route('/profile/<int:user_id>')
+def profile_SQL(user_id):
+    conn = sqlite3.connect('users.db')
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users WHERE id = ?', (user_id,))
+    user = cur.fetchone()
+    conn.close()
+    
+    return render_template('profile.html', user=user)
 
 
