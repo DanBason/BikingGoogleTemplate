6b5f1f8794f4ab58e0899af5a2dca9782daf5f52
Daniel Bason <dbason@gmail.com>
2023-03-26 11:09:15 -0700

    footer

--- app/routes/college.py
+++ app/routes/college.py
@@ -2,8 +2,8 @@ from app import app
 import mongoengine.errors
 from flask import render_template, flash, redirect, url_for
 from flask_login import current_user
-from app.classes.data import Recipe, Comment
-from app.classes.forms import RecipeForm, CommentForm
+from app.classes.data import College, Comment
+from app.classes.forms import CollegeForm, CommentForm
 from flask_login import login_required
 import datetime as dt
 
